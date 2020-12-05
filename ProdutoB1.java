bb840a590d3a3ef2d7d823ed304c37849a0f1d29
Geovanni99 <55093310+Geovanni99@users.noreply.github.com>
2020-12-05 17:32:39 -0300

    Delete ProdutoB1.java

--- ProdutoB1.java
+++
@@ -1,8 +0,0 @@
-public class ProdutoB1 implements ProdutoAbstratoB {
-
-    @Override
-    public void interagir(ProdutoAbstratoA a) {
-        System.out.println(this.getClass().getName() + " interage com " + a.getClass().getName());
-    }
-
-}
